fadd738705853d68bc56f92ddd5ded4dc1244f28
Jürgen Sieben <j.sieben@condes.de>
2018-03-21 14:30:06 +0000

    Bugfix

--- CodeGenerator/sql/create_messages.sql
+++ CodeGenerator/sql/create_messages.sql
@@ -1,10 +1,23 @@
-begin
+begin  
   pit_admin.merge_message(
     p_pms_name => 'CODE_GEN_MISSING_ANCHORS',
-    p_pms_text => q'^Bei Ausf�hrung des CodeGenerators fehlten folgende Ersetzungsanker: #1#^',
+    p_pms_text => q'^Bei Ausführung des CodeGenerators fehlten folgende Ersetzungsanker: #1#^',
+    p_pms_pse_id => 20,
+    p_pms_pml_name => 'GERMAN'); 
+    
+  pit_admin.merge_message(
+    p_pms_name => 'NO_TEMPLATE',
+    p_pms_text => q'^Es wurde kein Template übergeben^',
     p_pms_pse_id => 20,
     p_pms_pml_name => 'GERMAN');
-
+    
+    
+  pit_admin.merge_message(
+    p_pms_name => 'PASS_INFORMATION',
+    p_pms_text => q'^Das Template #1# ist nicht vorhanden^',
+    p_pms_pse_id => 70,
+    p_pms_pml_name => 'GERMAN'); 
+    
   pit_admin.create_message_package;
 end;
 /
